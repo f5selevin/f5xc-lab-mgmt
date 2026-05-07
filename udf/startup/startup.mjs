85aa36a29281556060fd67b0401d6cccc6f1b35a
sorinboia <sorinboia@gmail.com>
2026-05-07 09:47:15 +0300

    Update F5 XC Lab Management domain URL

--- udf/startup/startup.mjs
+++ udf/startup/startup.mjs
@@ -1,6 +1,6 @@
 import setupAutomation from './setupAutomation.mjs';
 
-const f5xcLabMgmtDomain = 'https://f5xclabmgmt.emea-ent.f5demos.com';
+const f5xcLabMgmtDomain = 'https://f5xclabmgmt.emea.f5demos.com';
 
 const args = process.argv.slice(2);
 const course = args[0];
